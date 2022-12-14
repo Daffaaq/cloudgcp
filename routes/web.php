5b7ea9768e73a6c6a976f65d07e3be66c84742f5
Daffaaq <daffaaqila48@gmail.com>
2022-12-14 15:07:17 +0700

    fix auth

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/mysql', function () {
+    Artisan::call('migrate:fresh', ['--force' => true]);
+    Artisan::call('db:seed', ['--force' => true]);
+});
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::group(['middleware' => ['auth']], function() {
     Route::resource('tables', TableController::class);
