f2c53e2248db79b378fde07a12fa5afe5be49331
Daffaaq <daffaaqila48@gmail.com>
2022-12-14 15:12:15 +0700

    fix_auth3

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/mysql', function () {
-    // Artisan::call('migrate:rollback', ['--force' => true]);
+    Artisan::call('migrate:rollback', ['--force' => true]);
     Artisan::call('migrate', ['--force' => true]);
     Artisan::call('db:seed', ['--force' => true]);
 });
